85a39878a6b3b687a32b213b3d1f8a00c2587d61
Abhishek Choudhary <theabhishek.srm@gmail.com>
2023-08-08 23:08:09 +0530

    hello

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const Hello = ()=>{
+    let batchName =  "UnStopable"
+
+    let styleObj = {color: "red",textAlign: "center"}
+
+    return (
+        <div className="section" >
+            <h1 style= {styleObj} > Hello World</h1> 
+            <p> Welcome to the class. - {batchName}</p>
+            {/* curely braces can access JS elements */}
+
+            <p> {styleObj.color} </p>
+           
+            
+        </div>
+    )
 }
-
-export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+//modules:
+import React from "react"  
+import ReactDOM from "react-dom" 
+import "./style.css"
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// function hello(){
+//     return "hello world"
+// }
+// hello()
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+// html, JSX => HTML + some features of JS
+
+
+// v-17
+ReactDOM.render( <Hello /> ,  document.getElementById("root"))
+
+
+
+// How to call a component: 
+// 1. <Hello />
+// 2. <Hello> </Hello>
